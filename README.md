ddff6c81d94a8b423204d18cced22b343b170f77
TheEndBoss_101 <TheEndBoss101@gmail.com>
2023-12-03 21:17:12 -0600

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# AdvArcRadio
+The server componet for AdvArcRad  
+ALL AUDIO MUST BE 44100 hz
---
+++ main.lua
@@ -0,0 +1,20 @@
+local MainModule = {
+	["Main"] = "gm13/radio/main.wav";
+	["Static"] = {
+		"ambient/levels/prison/radio_random1.wav";
+		"ambient/levels/prison/radio_random2.wav";
+		"ambient/levels/prison/radio_random3.wav";
+		"ambient/levels/prison/radio_random4.wav";
+		"ambient/levels/prison/radio_random5.wav";
+		"ambient/levels/prison/radio_random6.wav";
+		"ambient/levels/prison/radio_random7.wav";
+	};
+	["SpecialRandom1"] = 1;
+	["SpecialRandom2"] = 100;
+	["SpecialChance"] = 50;
+	["MainRandom1"] = 1;
+	["MainRandom2"] = 100;
+	["MainChance"] = 25;
+}
+
+return MainModule
