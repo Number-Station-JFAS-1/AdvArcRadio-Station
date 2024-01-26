066f62a2ec981789925a478a4086c26e18887f8e
TheEndBoss_101 <94846253+TheEndBoss-101@users.noreply.github.com>
2024-01-26 17:55:55 -0600

    Update Main_SETTINGS.lua

--- sounds/Main_SETTINGS.lua
+++ sounds/Main_SETTINGS.lua
@@ -1,16 +1,30 @@
- local MainModule = {
-	["T1"] = {
-		["sstv/flags"] = {
-            ["Mode"] = "Random";
-            ["Random1"] = 50;
-            ["Random2"] = 1;
-            ["Chance"] = 10;
-            "/EKG.wav"; --ekg
-            "/AQRF.wav"; --aqrf
-            "/ARC.wav"; --arc
-            "/Eagle_Detector.wav"; --ED
-        };
-    };
+local sample_table_Chance = {
+	["Mode"] = "Chance",
+    ["Random1"] = 1,
+    ["Random2"] = 100,
+    ["Chance"] = 50,
+	"/Sound1.wav",
+	"/Sound2.wav",
+	"/Sound3.wav",
+}
+local sample_table_Random = {
+	["Mode"] = "Random",
+	"/Sound1.wav",
+	"/Sound2.wav",
+	"/Sound3.wav",
+}
+
+
+local sample_tier = {
+	["sstv/flags"] = sample_table_Random;
+	["sstv/1"] = sample_table_Random;
+	["custom"] = sample_table_Chance;
+}
+
+local MainModule = {
+	["T1"] = sample_tier;
+	["T2"] = sample_tier;
+	["T3"] = sample_tier;
     ["Weights"] = {
 		["T1"] = 10;
 		["T2"] = 5;
